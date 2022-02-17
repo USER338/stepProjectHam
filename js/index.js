d5069af5eded44d801f696f41b00942d60a8f464
Andrey  Tancura <7115839-TOSTcR@users.noreply.gitlab.com>
2022-02-17 13:25:24 +0200

    complete section 2,3

---
+++ js/index.js
@@ -0,0 +1,22 @@
+const tabsBtn = document.querySelectorAll('.tabs-title')
+const tabsItem = document.querySelectorAll('.tabs-item')
+
+tabsBtn.forEach(element => {
+	element.addEventListener('click', () => {
+		let currentBtn = element
+		let tabId = currentBtn.getAttribute('data-tab')
+		let currentTab = document.querySelector(tabId)
+		if (!currentBtn.classList.contains('active')) {
+			tabsBtn.forEach(element => {
+				element.classList.remove('active')
+			})
+
+			tabsItem.forEach(element => {
+				element.classList.remove('active')
+			})
+
+			currentBtn.classList.add('active')
+			currentTab.classList.add('active')
+		}
+	})
+})
