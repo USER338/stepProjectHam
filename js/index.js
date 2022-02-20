fa767c9b83ce7eec7ea787969dc9162f8e5dc957
Andrey  Tancura <7115839-TOSTcR@users.noreply.gitlab.com>
2022-02-20 20:43:33 +0200

    add 2,3 section + logic

--- js/index.js
+++ js/index.js
@@ -21,3 +21,46 @@ tabsBtn.forEach(element => {
 		}
 	})
 })
+
+/* WORKS-TABS */
+
+const worksBtn = document.querySelectorAll('.works-tabs__title')
+const worksItem = document.querySelectorAll('.works-tabs__item')
+const worksContent = document.querySelector('.works-tabs__content')
+
+worksBtn.forEach(element => {
+	element.addEventListener('click', () => {
+		let currentBtn = element
+		let tabId = currentBtn.getAttribute('data-tab')
+		let currentTab = document.querySelector(tabId)
+		if (!currentTab.classList.contains('active')) {
+			worksBtn.forEach(element => {
+				element.classList.remove('active')
+			})
+
+			worksItem.forEach(element => {
+				element.classList.remove('active')
+			})
+
+			currentBtn.classList.add('active')
+			currentTab.classList.add('active')
+		}
+	})
+})
+
+/* LOAD MORE */
+
+const btn = document.querySelector('.works-more')
+const hidden = document.querySelector('.works-tabs__item--hidden')
+const loader = document.querySelector('.lds-ripple')
+btn.addEventListener('click', () => {
+	let timerId = setTimeout(function showLoader() {
+		loader.classList.add('active')
+		btn.style.display = 'none'
+	}, 2000)
+
+	setTimeout(() => {
+		loader.classList.remove('active')
+		hidden.classList.add('active')
+	}, 5000)
+})
